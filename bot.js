313f3bb270b6d5c37c07da43c496cf98e22212fb
Alados5 <adria.alados@gmail.com>
2018-01-18 01:41:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var fs = require("fs");
 
-var path = './IDSave.txt';
-var text = 'testtext';
-
-//fs.readFile(path, "utf8", function(error, data) {
-//    if(error) {
-//        console.error("Read error: " + error.message);
-//    }
-//    else {
-//        text = data;
-//    }
-//});
+list = [];
+i = 0;
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply(text);
-        fs.appendFile(path, 'pong', function(error) {
-            if(error) {
-                console.error("Write error: "+error.message);
-            }
-        });
+    if (message.content === 'Bot') {
+    	message.reply('Hey');
+        list[i] = 'xd';
+        i+=1;
   	}
 });
 
