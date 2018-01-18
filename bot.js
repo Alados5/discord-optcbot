2bb0a9ceaed83c2c5cefbb8cc500d66bdfad2bd8
Alados5 <adria.alados@gmail.com>
2018-01-18 19:21:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const client = new Discord.Client();
 
 var alist = {};
 var i = 1;
-var path = "./Test.txt"
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -27,14 +26,27 @@ client.on('message', message => {
         var database = '';
         for(var key in alist) {
             if (alist.hasOwnProperty(key)) {
-                database += key+' ; ';
+                database += key + ' ; ';
                 database += alist[key]['ID'] + ' ; ';
                 database += alist[key]['Server'] + ' ; ';
                 database += alist[key]['Info'] + ' ; ';
-                database += alist[key]['Link'] + '\n';
+                database += alist[key]['Link'] + ' ;; \n';
             } 
         }
         message.channel.send(database);
+    }
+    
+    if (message.content.split(' ')[0] === 'Preload') {
+        message.reply("Preloading");
+        
+        var lines = message.content.split(';;');
+        for(i=0; i<lines.length; i++) {
+            var parts = lines.split(' ; ');
+            var useri = parts[0];
+            alist[useri] = {'ID':parts[1], 'Server':parts[2], 'Info':parts[3], 'Link':parts[4]};
+        }
+        
+        message.channel.send("Update complete!");
     }
     
 });
