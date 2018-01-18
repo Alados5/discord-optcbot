b43a70c26aaea6e67b9c793735bdfc27d96df894
Alados5 <adria.alados@gmail.com>
2018-01-18 19:38:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if(msg.author.bot) return;
+    if(message.author.bot) return;
         
     if (message.content === 'Bot') {
         
