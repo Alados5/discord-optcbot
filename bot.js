d6ac44f71c8d149b3adbfd2da9ac2f76bda97ef0
Alados5 <adria.alados@gmail.com>
2018-01-21 00:11:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,63 +2,28 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 //var fs = require("fs");
 
-var alist = {};
-var i = 1;
+var prefix = '!';
+var dbchar = 'http://optc-db.github.io/characters/#/search/';
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+//client.on('ready', () => {
+//  console.log('I am ready!');
+//});
 
-client.on('message', message => {
-    if(message.author.bot) return;
-        
-    if (message.content === 'Bot') {
-        
-        message.reply('Working on it!');
-        
-        var useri = 'User'+i.toString()
-        alist[useri] = {'ID':(100000001*i).toString(), 'Server':"Japan", 'Info':"Some text here", 'Link':"https://imgur.com"};
-        
-        message.reply(alist[useri]['ID']);
-        
-        i++;
-    }
+client.on('message', msg => {
+  if(msg.author.bot || !msg.content.startsWith(prefix)) return;
     
-    if (message.content === 'AllData') {
-        var database = '';
-        for(var key in alist) {
-            if (alist.hasOwnProperty(key)) {
-                database += key + ' ; ';
-                database += alist[key]['ID'] + ' ; ';
-                database += alist[key]['Server'] + ' ; ';
-                database += alist[key]['Info'] + ' ; ';
-                database += alist[key]['Link'] + ';; \n';
-            } 
-        }
-        message.channel.send(database);
-    }
+  //Handles arguments to just take the first word
+  const args = msg.content.slice('!'.length).split(/ +/);
+  const command = args.shift().toLowerCase(); 
+        
+  if (command == 'thejoselu8') {
+    msg.reply('TIIIIOOOO!!!!!')
+  }
     
-    var ispreload = (message.content.slice(0, 7) == "Preload");
-    if (ispreload) {
-        message.reply("Preloading");
-        
-        var bulkdata = message.content.slice(8);
-        var lines = bulkdata.split(';;');
-        
-        for(j=0; j<lines.length; j++) {
-            var parts = lines[j].split(' ; ');
-            
-            var useri = parts[0]
-            useri = useri.replace('\n', '');
-            if (useri.slice(0,1) == ' ') {
-                useri = useri.slice(1);
-            }    
-            
-            alist[useri] = {'ID':parts[1], 'Server':parts[2], 'Info':parts[3], 'Link':parts[4]};
-        }
-        
-        message.channel.send("Update complete!");
-    }
+  if (command == 'char' || command == 'pj') {
+    var chartolook = args.toString();
+    msg.channel.send(dbchar+chartolook)
+  } 
     
 });
 
