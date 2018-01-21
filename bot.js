cdeb4d5efb734547c9da7c72e08d5b85ee8b23a1
Alados5 <adria.alados@gmail.com>
2018-01-21 01:27:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,23 +6,38 @@ var prefix = '!';
 var dbchar = 'http://optc-db.github.io/characters/#/search/';
 var dbcharid = 'http://optc-db.github.io/characters/#/view/';
 
-//client.on('ready', () => {
-//  console.log('I am ready!');
-//});
-
 client.on('message', msg => {
   if(msg.author.bot || !msg.content.startsWith(prefix)) return;
     
   //Handles arguments to just take the first word
   const args = msg.content.slice('!'.length).split(/ +/);
   const command = args.shift().toLowerCase(); 
-        
+
+
+//------------------------------------------------------------------------- START HELP  
+  
+  if (command == 'ayuda') {
+    var helptext = '¡Asistente automático en marcha! Comandos:'+
+        '\n !ayuda - activa esto'+
+        '\n !pj o !char - busca un personaje en la database (nombre o #ID)'+
+        '\n !thejoselu8 - cabrea a JoseLu gratis'
+    
+    msg.channel.send(helptext)
+  }  
+  
+//------------------------------------------------------------------------- END HELP    
+  
+//------------------------------------------------------------------------- START MEMES
+
   if (command == 'thejoselu8') {
     msg.reply('TIIIIOOOO!!!!!')
   }
-    
-  if (command == 'char' || command == 'pj') {
-    
+
+//------------------------------------------------------------------------- END MEMES  
+  
+//------------------------------------------------------------------------- START CHAR/PJ    
+  
+  if (command == 'char' || command == 'pj') {   
     var chartolook = args.toString();
         
     var isaname = isNaN(parseInt(chartolook));
@@ -32,7 +47,9 @@ client.on('message', msg => {
     }
     else msg.channel.send(dbcharid+chartolook);
   } 
-    
+
+//------------------------------------------------------------------------- END CHAR/PJ   
+  
 });
 
 // THIS  MUST  BE  THIS  WAY
