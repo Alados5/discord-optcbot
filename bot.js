95102b3f6f9136a412cb56b43f10cca328bfc4e3
Alados5 <adria.alados@gmail.com>
2018-01-24 04:20:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -133,6 +133,7 @@ client.on('message', msg => {
                    "\n \n Me puedes llamar con estos comandos:"+
                    "\n   **!ayuda** - Muestra este mensaje"+
                    "\n   **!char** o **!pj** - Busca un personaje o ID en la database"+
+                   "\n   **!skillup K N - Calcula la probabilidad de obtener K niveles con N copias"+
                    "\n   **!thejoselu8** - Cabrea a joselu ¡Gratis!";
     
     msg.channel.send({embed: {
@@ -239,6 +240,7 @@ client.on('message', msg => {
     while(copies<200) {
       var realprob = ProbSkill(k, copies, Event, OC);
       if(realprob >= x) { break; }
+      msg.channel.send(copies+" copies. Prob: "+realprob)
       copies++;
     }
     
