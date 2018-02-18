705405f5fff2746c481ea0b03a10e33dd65425f4
Alados5 <adria.alados@gmail.com>
2018-02-18 02:29:57 +0100

    Update details0.js

--- details0.js
+++ details0.js
@@ -1,3 +1,13 @@
-var db0 = "test text";
+var db0 = {
+    1: {
+        special: "Deals 5x character's ATK in STR damage to one enemy",
+        specialName: "Spinning Gum Punch"
+    },
+    2: {
+        special: "Deals 7x character's ATK in STR damage to one enemy",
+        specialName: "Gum-Gum Pistol",
+        captain: "Boosts ATK of all characters by 1.5x"
+    },
+};
 
 module.exports.db0 = db0;
