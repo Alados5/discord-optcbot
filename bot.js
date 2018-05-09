d237289b9502e92ee03b90210a4fccd87b519e34
Alados5 <adria.alados@gmail.com>
2018-05-09 13:25:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -949,15 +949,15 @@ client.on('message', msg => {
     var randnum = Math.random()*100;
     var pulls = [];
     msg.channel.send(randnum)
-    for (var chname in sugolist) {
-      if (dic.hasOwnProperty(chname)) {
-        var prange = sugolist[chname];
-        if (randnum >= prange[0] && randnum < prange[1]) {
-          pulls.push(chname);
-        }
-      }
-    }
-    msg.channel.send(pulls)  
+    //for (var chname in sugolist) {
+    //  if (dic.hasOwnProperty(chname)) {
+    //    var prange = sugolist[chname];
+    //    if (randnum >= prange[0] && randnum < prange[1]) {
+    //      pulls.push(chname);
+    //    }
+    //  }
+    //}
+    //msg.channel.send(pulls)  
       
   }
     
