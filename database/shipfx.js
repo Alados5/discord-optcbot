ef8c32b733214437f6b84294e10c277a83428cb8
Alados5 <adria.alados@gmail.com>
2018-05-18 19:09:30 +0200

    Update shipfx.js

--- database/shipfx.js
+++ database/shipfx.js
@@ -1,4 +1,168 @@
 var shipfx = {
+    0: {
+        name: "Dinghy",
+        effect: "--"
+    },
+    1: {
+        name: "Going Merry",
+        effect: "--"
+    },
+    2: {
+        name: "Navy Ship",
+        effect: "--"
+    },
+    3: {
+        name: "Baratie",
+        effect: "--"
+    },
+    4: {
+        name: "Coffin Boat",
+        effect: "--"
+    },
+    5: {
+        name: "Miss Love Duck",
+        effect: "--"
+    },
+    6: {
+        name: "Flying Merry",
+        effect: "--"
+    },
+    7: {
+        name: "Moby Dick",
+        effect: "--"
+    },
+    8: {
+        name: "Big Top",
+        effect: "--"
+    },
+    9: {
+        name: "Bezan Black",
+        effect: "--"
+    },
+    10: {
+        name: "Aokiji's Bike",
+        effect: "--"
+    },
+    11: {
+        name: "Ace's Striker",
+        effect: "--"
+    },
+    12: {
+        name: "Dreadnaught Sabre",
+        effect: "--"
+    },
+    13: {
+        name: "Thousand Sunny",
+        effect: "--"
+    },
+    14: {
+        name: "Kuja Pirate Ship",
+        effect: "--"
+    },
+    15: {
+        name: "Ark Maxim",
+        effect: "--"
+    },
+    16: {
+        name: "Red Force",
+        effect: "--"
+    },
+    17: {
+        name: "2nd Anniversary Thousand Sunny",
+        effect: "--"
+    },
+    18: {
+        name: "Sun Pirates Ship",
+        effect: "--"
+    },
+    19: {
+        name: "Donquixote Family Ship",
+        effect: "--"
+    },
+    20: {
+        name: "Donquixote Family Ship - Special Activated",
+        effect: "--"
+    },
+    21: {
+        name: "Burning Moby Dick",
+        effect: "--"
+    },
+    22: {
+        name: "Rocketman",
+        effect: "--"
+    },
+    23: {
+        name: "Garp Ship",
+        effect: "--"
+    },
+    24: {
+        name: "Polar Tang",
+        effect: "--"
+    },
+    25: {
+        name: "New Year Big Top",
+        effect: "--"
+    }, 
+    26: {
+        name: "Coated Thousand Sunny",
+        effect: "--"
+    }, 
+    27: {
+        name: "Kizaru's Cannonball",
+        effect: "--"
+    }, 
+    28: {
+        name: "Going Luffy Senpai!",
+        effect: "--"
+    }, 
+    29: {
+        name: "Thriller Bark",
+        effect: "--"
+    }, 
+    30: {
+        name: "Karasumaru Ship",
+        effect: "--"
+    }, 
+    31: {
+        name: "Karasumaru Ship - Special Activated",
+        effect: "--"
+    }, 
+    32: {
+        name: "3rd Anniversary Thousand Sunny",
+        effect: "--"
+    }, 
+    33: {
+        name: "Flying Dutchman",
+        effect: "--"
+    }, 
+    34: {
+        name: "Flying Dutchman - Special Activated",
+        effect: "--"
+    }, 
+    35: {
+        name: "Marshall D. Teach Raft",
+        effect: "--"
+    }, 
+    36: {
+        name: "Revolutionary Black Crow",
+        effect: "--"
+    }, 
+    37: {
+        name: "Zunisha",
+        effect: "--"
+    }, 
+    38: {
+        name: "Laboon",
+        effect: "--"
+    }, 
+    39: {
+        name: "Sexy Foxy",
+        effect: "--"
+    }, 
+    40: {
+        name: "--",
+        effect: "--"
+    }
 
 };
 
