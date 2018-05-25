8811532027aad3ba967254044fff66017c32a59e
Alados5 <adria.alados@gmail.com>
2018-05-25 18:46:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -411,10 +411,7 @@ client.on('message', msg => {
       var ntoclear = parseInt(args[0]);
       if (!ntoclear || isNaN(ntoclear)) return msg.reply("You didn't say how many messages mate!")
       msg.delete()
-      const fetched = await msg.channel.fetchMessages({limit: ntoclear});
-      msg.channel.bulkDelete(fetched)
-        .catch(error => msg.channel.send(`Error: ${error}`));
-      
+      msg.channel.bulkDelete(ntoclear);
     }
       
       
