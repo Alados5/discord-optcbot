b5869bf7d81774a4c37ae8a74b04e780fe310c6d
Alados5 <adria.alados@gmail.com>
2020-02-08 18:23:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -365,9 +365,9 @@ client.on('ready', () => {
 
 
 client.on('guildMemberAdd', member => {
-  var channel = member.guild.channels.get(613017080668749835);
+  var channel = member.guild.channels.find('name', 'general');
   if (!channel) return;
-  //if (channel.id == 613017080668749835) {
+  if (channel.id == 613017080668749835) {
       channel.send(`Muy buenas ${member}, ¿qué tal andamos? ¡Bienvenido al Server!`);
   }
 });
